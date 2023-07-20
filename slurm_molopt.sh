edbcf9d894d17569a9bcbb0379eeeba0e360d6eb
arunraja-hub <surviva614@gmail.com>
2023-07-20 17:01:24 +0100

    bash

--- slurm_molopt.sh
+++ slurm_molopt.sh
@@ -1,21 +1,22 @@
-#!/bin/bash   
-#SBATCH -J molopt                     # Job name
-#SBATCH --time=48:00:00                 # Walltime                                      
-#SBATCH --mem-per-cpu=12000            # memory/cpu (in MB) ### commented out              
-#SBATCH --ntasks=1                      # 1 tasks                                               
-#SBATCH --cpus-per-task=1           # number of cores per task                          
-#SBATCH --gpus-per-task=1           # number of cores per task                          
-#SBATCH --nodes=1                       # number of nodes                                       
+#!/bin/bash
+#SBATCH -J molopt
+#SBATCH --time=48:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-gpu=1
+#SBATCH --mem-per-cpu=12000                                    
 #S BATCH --exclusive                     # node should not be shared with other jobs, only use this if you intend the node to be usable only by you as this will block other users from submitting jobs     to the same node                
 #SBATCH --chdir=/vols/opig/users/raja/deep-molecular-optimization # From where you want the job to be run
 #SBATCH --mail-user=arun.raja@dtc.ox.ac.uk  # set email address                           
 #SBATCH --mail-type=ALL                 # Spam us with everything, caution
 #SBATCH --mail-type=begin               # Instead only email when job begins...
 #SBATCH --mail-type=end                 # ... and ends
-#SBATCH --partition=nagagpu04-high-debug  # Select a specific partition rather than default 
-#SBATCH --clusters=all
+#SBATCH --partition=high-opig-gpu  # Select a specific partition rather than default 
+#SBATCH --clusters=srf_gpu_01
 #SBATCH --partition=high-opig-test    # Select a specific partition rather than default
-#S BATCH -w nagagpu04.cpu.stats.ox.ac.uk # Provide a specific node/nodelist rather than the standard nodelist associated with the partition (useful if you have a data setup on one specific node)
+#SBATCH -w nagagpu04.cpu.stats.ox.ac.uk # Provide a specific node/nodelist rather than the standard nodelist associated with the partition (useful if you have a data setup on one specific node)
 #SBATCH --output=/vols/opig/users/raja/slurm_outs/slurm_%j.out  # Writes standard output to this file. %j is jobnumber                             
 #SBATCH --error=/vols/opig/users/raja/slurm_outs/slurm_%j.err   # Writes error messages to this file. %j is jobnumber
 echo $CUDA_VISIBLE_DEVICES 
