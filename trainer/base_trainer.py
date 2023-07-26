3d441d253a8ef4d888145e52c11f8ef8f7ed944e
arunraja-hub <surviva614@gmail.com>
2023-07-26 18:28:20 +0100

    new name for tb based on opt

--- trainer/base_trainer.py
+++ trainer/base_trainer.py
@@ -15,7 +15,7 @@ class BaseTrainer(ABC):
     def __init__(self, opt):
 
         self.save_path = os.path.join('experiments', opt.save_directory)
-        self.summary_writer = SummaryWriter(logdir=os.path.join(self.save_path, 'tensorboard'))
+        self.summary_writer = SummaryWriter(logdir=os.path.join(self.save_path, 'tensorboard'+str(opt)))
         LOG = ul.get_logger(name="train_model", log_path=os.path.join(self.save_path, 'train_model.log'))
         self.LOG = LOG
         self.LOG.info(opt)
