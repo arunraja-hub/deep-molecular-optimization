f43f4472b942210d207280ea16ea3a07e561c13a
arunraja-hub <surviva614@gmail.com>
2023-08-14 14:53:42 +0100

    optimise based on val acc

--- train_molopt.sh
+++ train_molopt.sh
@@ -6,7 +6,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --gpus-per-task=1
 #SBATCH --cpus-per-gpu=1
-#SBATCH --mem-per-cpu=96000
+#SBATCH --mem-per-cpu=36000
 #S BATCH --exclusive
 #SBATCH --chdir=/vols/opig/users/raja/deep-molecular-optimization
 #SBATCH --mail-user=arun.raja@dtc.ox.ac.uk
