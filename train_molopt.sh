806d5d28c7d1440b70d8de7b556811ac785a6aa9
arunraja-hub <surviva614@gmail.com>
2023-09-07 18:21:50 +0100

    multiple gpu

--- train_molopt.sh
+++ train_molopt.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=96:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:3
 #SBATCH --gpus-per-task=3
 #SBATCH --cpus-per-gpu=4
 #SBATCH --mem-per-cpu=24000
