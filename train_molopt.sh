4ce24466e147daed4630caac9e016f1c0af02103
Arun Raja <shil5919@arc-login02.arcus-htc.arc.local>
2023-09-09 13:21:55 +0100

    no optuna

--- train_molopt.sh
+++ train_molopt.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-#SBATCH -J molopt
+#SBATCH -J molopt-no-optuna
 #SBATCH --time=96:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
 #SBATCH --gpus-per-task=1
 #SBATCH --cpus-per-gpu=4
-#SBATCH --mem-per-cpu=24000
+#SBATCH --mem-per-cpu=36000
 #S BATCH --exclusive
-#SBATCH --chdir=/vols/opig/users/raja/deep-molecular-optimization
+#SBATCH --chdir=/home/shil5919/opig/deep-molecular-optimization
 #SBATCH --mail-user=arun.raja@dtc.ox.ac.uk
 #SBATCH --mail-type=ALL
 #SBATCH --mail-type=begin
 #SBATCH --mail-type=end
-#SBATCH --partition=high-opig-gpu
-#SBATCH --clusters=srf_gpu_01
-#SBATCH --partition=high-opig-test
-#SBATCH -w nagagpu04.cpu.stats.ox.ac.uk
-#SBATCH --output=/vols/opig/users/raja/molopt_slurm/slurm_%j.out                             
-#SBATCH --error=/vols/opig/users/raja/molopt_slurm/slurm_%j.err
+#S BATCH --partition=high-opig-gpu
+#SBATCH --qos=standard
+#S BATCH --partition=high-opig-test
+#S BATCH -w nagagpu04.cpu.stats.ox.ac.uk
+#SBATCH --output=/home/shil5919/opig/slurm_molopt/slurm_%j.out
+#SBATCH --error=/home/shil5919/opig/slurm_molopt/slurm_%j.err
 
 echo $CUDA_VISIBLE_DEVICES 
 echo $CONDA_DEFAULT_ENV
@@ -27,7 +27,11 @@ for dir in */; do
 done
 #python -m venv gin7
 #source gin7/bin/activate
-source activate molfeat
+module load Anaconda3/2022.10
+conda activate /data/stat-cadd/shil5919/molfeat
+pip3 install torch torchvision torchaudio
+pip install molfeat[graphormer]
+pip install numpy
 echo $CONDA_DEFAULT_ENV
 echo "training"
-python train.py --batch-size 16 --data-path data/chembl_02 --save-directory train_transformer_molopt_original --model-choice transformer --num-epoch 30 transformer
+python train.py --data-path data/chembl_02 --save-directory train_transformer_molopt_original --model-choice transformer --num-epoch 30 transformer
