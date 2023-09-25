498baadca445481bc39679b6de85b03e2b48a3a4
Arun Raja <shil5919@htc-login01.arcus-htc.arc.local>
2023-09-25 13:55:20 +0100

    pcqm

--- train.py
+++ train.py
@@ -4,10 +4,6 @@ import configuration.opts as opts
 from trainer.transformer_trainer import TransformerTrainer
 from trainer.seq2seq_trainer import Seq2SeqTrainer
 
-
-import optuna
-from optuna.trial import TrialState
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='train.py',
@@ -17,32 +13,7 @@ if __name__ == "__main__":
     opt = parser.parse_args()
 
     if opt.model_choice == 'transformer':
-        def objective(trial):
-            trainer = TransformerTrainer(opt)
-            loss_epoch_train, loss_epoch_validation, accuracy = trainer.train(opt=opt,trial=trial)
-            return accuracy
-    # elif opt.model_choice == 'seq2seq':
-    #     def objective(trial):
-    #         trainer = Seq2seqt(opt)
-    #         trainer.train(opt=opt,trial=trial)
-
-    study = optuna.create_study(study_name="train_model-switch-source-target", direction="maximize", 
-        pruner=optuna.pruners.MedianPruner(n_startup_trials=2, n_warmup_steps=30, interval_steps=10))
-    study.optimize(objective, n_trials=5)
-
-    pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
-    complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
-
-    print("Study statistics: ")
-    print("  Number of finished trials: ", len(study.trials))
-    print("  Number of pruned trials: ", len(pruned_trials))
-    print("  Number of complete trials: ", len(complete_trials))
-
-    print("Best trial:")
-    trial = study.best_trial
-
-    print("  Value: ", trial.value)
-
-    print("  Params: ")
-    for key, value in trial.params.items():
-        print("    {}: {}".format(key, value))
+        trainer = TransformerTrainer(opt)
+    elif opt.model_choice == 'seq2seq':
+        trainer = Seq2SeqTrainer(opt)
+    trainer.train(opt)
--- train_molopt.sh
+++ train_molopt.sh
@@ -4,9 +4,9 @@
 #SBATCH --partition=short
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --gres=gpu:3
-#SBATCH --gpus-per-task=3
-#SBATCH --cpus-per-gpu=4
+#SBATCH --gres=gpu:4
+#SBATCH --gpus-per-task=4
+#SBATCH --cpus-per-gpu=1
 #SBATCH --mem-per-cpu=36000
 #S BATCH --exclusive
 #SBATCH --chdir=/home/shil5919/opig/deep-molecular-optimization
@@ -20,7 +20,6 @@
 #S BATCH -w nagagpu04.cpu.stats.ox.ac.uk
 #SBATCH --output=/home/shil5919/opig/slurm_molopt/slurm_%j.out
 #SBATCH --error=/home/shil5919/opig/slurm_molopt/slurm_%j.err
-
 echo $CUDA_VISIBLE_DEVICES 
 echo $CONDA_DEFAULT_ENV
 for dir in */; do
@@ -28,12 +27,12 @@ for dir in */; do
 done
 #python -m venv gin7
 #source gin7/bin/activate
-module load Anaconda3/2022.10
-conda activate /data/stat-cadd/shil5919/molfeat
-pip3 install torch torchvision torchaudio
-pip install molfeat[graphormer]
-pip install molfeat[transformer]
-pip install numpy
+# module load Anaconda3/2022.10
+# conda activate /data/stat-cadd/shil5919/molfeat
+# pip3 install torch torchvision torchaudio
+# pip install molfeat[graphormer]
+# pip install molfeat[transformer]
+# pip install numpy
 echo $CONDA_DEFAULT_ENV
 echo "training"
 python train.py --data-path data/chembl_02 --save-directory train_transformer_molopt_original --model-choice transformer --num-epoch 30 transformer
